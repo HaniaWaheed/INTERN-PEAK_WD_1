869ad72f4349e9654f20739eee528015d0c45e33
HaniaWaheed <haniawaheed2000@gmail.com>
2024-07-25 19:17:34 +0500

    assignment 3 oop

---
+++ categories.css/categories.css/Program.cs
@@ -0,0 +1,61 @@
+﻿
+ul
+{
+    list - style - type: none;
+margin: 0;
+padding: 0;
+    background - color: tomato;
+}
+li
+{
+    float: left;
+}
+
+li a
+{
+    font-size: 25px;
+    font-family : "Copperplate",fantasy;
+    background-color: tomato;
+    display:  ;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover
+{
+    background - color: red;
+}
+body
+{
+    background - image: linear - gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),url("store.jpg");
+    background - size: cover;
+    background - position:fixed;
+}
+h1
+{
+    font - family: "Copperplate FS",fantasy;
+color: tomato;
+    text - decoration: overline;
+    font - size: 650 %;
+
+    text - align: center;
+    padding - top: 3 %;
+}
+h3
+{
+    font - family:copperplate,fantasy;
+    font - size:175 %;
+    font - style:normal;
+color: tomato;
+display: inline;
+}
+h4
+{
+    font - family:copperplate,fantasy;
+    font - size:200 %;
+    font - style:normal;
+color: burlywood;
+display: inline;
+}
